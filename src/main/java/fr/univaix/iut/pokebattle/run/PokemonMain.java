e0f063ee9faee292d4d1fbd8045958f912bd52e4
Munshausen <remy.elouard@gmail.com>
2013-04-04 17:12:50 +0200

    mise en place pattern, manque récupération tweet

--- src/main/java/fr/univaix/iut/pokebattle/run/PokemonMain.java
+++ src/main/java/fr/univaix/iut/pokebattle/run/PokemonMain.java
@@ -1,10 +1,33 @@
 package fr.univaix.iut.pokebattle.run;
 
-import fr.univaix.iut.pokebattle.bot.PokeBot;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class PokemonMain {
-    public static void main(String[] args) {
-    	//Regarder tweet, extraire nom pokemon, appeler properties qui va bien
-        BotRunner.runBot(new PokeBot(), "twitter4j.properties");
+import fr.univaix.iut.pokebattle.bot.PokeBot;
+import fr.univaix.iut.pokebattle.twitter.Tweet;
+
+public class PokemonMain 
+{
+	String Properties = new String ("twitter4j.properties");
+	
+	
+	public String Prop (Tweet question)
+	{
+		String NomPokemon = new String ();
+		Pattern pattern = Pattern.compile("@([^ ]+)");
+		Matcher matcher = pattern.matcher(question.getText());
+		if(matcher.find() && question.getScreenName() != null)
+		{
+			NomPokemon = matcher.group(1); 
+		}
+		
+		
+		return NomPokemon+".properties";
+	}
+	//Regarder tweet, extraire nom pokemon, appeler properties qui va bien
+
+	public static void main(String[] args) {
+    	
+        BotRunner.runBot(new PokeBot(), Prop(question)); // problème, comment récupérer le tweet de façon effective ?
     }
 }
