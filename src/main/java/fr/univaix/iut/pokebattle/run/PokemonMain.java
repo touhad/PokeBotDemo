fb516eeeb3014371711613694cc588d5e1b13271
Munshausen <remy.elouard@gmail.com>
2013-04-09 17:04:34 +0200

    générique, pas encore fonctionnel

--- src/main/java/fr/univaix/iut/pokebattle/run/PokemonMain.java
+++ src/main/java/fr/univaix/iut/pokebattle/run/PokemonMain.java
@@ -1,6 +1,7 @@
 package fr.univaix.iut.pokebattle.run;
 
 import java.io.File;
+import java.io.FilenameFilter;
 
 import fr.univaix.iut.pokebattle.bot.PokeBot;
 
@@ -8,9 +9,24 @@ public class PokemonMain
 {
 	
 	
+/*	File[] fichiersJava = res.listFiles(new FilenameFilter()
+	{
+		public boolean accept(File dir) 
+		{
+		  String name = new String();
+		  return name.endsWith(".properties");
+		}
+	});	  */  
     public static void main(String[] args) 
     {
-        BotRunner.runBot(new PokeBot(), "Magicarpe.properties");
-        BotRunner.runBot(new PokeBot(), "Ramoloss.properties");
+    	File res = new File("resources");
+    	for (File PropFile : res.listFiles())
+    	{
+    		String Prop = new String (PropFile.getName());
+    		BotRunner.runBot(new PokeBot(), Prop);
+    	
+    	//BotRunner.runBot(new PokeBot(), "Magicarpe.properties");
+        //BotRunner.runBot(new PokeBot(), "Ramoloss.properties");
+    	}
     }
 }
